e8ba2cb38ba2d7a8e2fae3390c2c57c9165a9741
ILYA655 <75269869+ILYA655@users.noreply.github.com>
2022-04-18 20:29:13 +0300

    Update Source.cpp

--- lab_4/Source.cpp
+++ lab_4/Source.cpp
@@ -1,85 +1,57 @@
-// Lab4.cpp: определяет точку входа для консольного приложения.
-//
-
-//#include "stdafx.h"
-/*#include <set>
-#include <iostream>
-using namespace std;
-
-void main()
-{
-	set <int> SetOne;
-	cout << "Enter the number of elements: ";
-	int n, ni;
-	cin >> n;
-	cout << endl;
-
-	for(int i = 0; i < n; i++){
-		int num;
-		cin >> num;
-		SetOne.insert(num);
-	}
-	set <int> :: iterator U = SetOne.begin();
-	for(int i = 0; i < SetOne.size(); i++){ cout << *U << " "; U++;}
-	cout << "Enter num for La'BAGET: " ;
-	cin >> n;
-
-	if(SetOne.find(n) != SetOne.end()){
-	SetOne.erase(n);
-	cout << endl << " La'BAGET was deleteeeeeeeeeeeeeeeeed";
-	set <int> :: iterator U = SetOne.begin();
-	for(int i = 0; i < SetOne.size(); i++){ cout << *U << " "; U++;}
-	}
-	else{
-	cout << endl << "La'BAGET does not exist";
-	}
-
-	system("pause");
-
-	return;
-}*/
 #include "Header.h"
-//int main() {
-//	setlocale(LC_ALL, "rus");
-//	int num;
-//	Set<int>TSet(1);
-//	int delNum;
-//	TSet.PrintSet();
-//	system("pause");
-//	cout << "Enter element to delete:" << endl;
-//	cin >> delNum;
-//	TSet - delNum;
-//	TSet.PrintSet();
-//	Set<int>PODSet(1);
-//	cout << endl << "Set 2 is multiplicity for Set 1: ";
-//	if (TSet>PODSet)
-//	cout  << "YES" << endl;
-//	else cout << "NO" << endl;
-//	cout << "Set 1 and Set 2 are not equal: ";
-//	if (TSet != PODSet)
-//		cout << "YES";
-//	else cout << "NO";
-//	cout << endl;
-//	cin >> num;
-//	TSet + num;
-//	cout << endl;
-//	TSet.PrintSet();
-//	system("pause");
-//	return 0;
-//}
-
 int main() {
-
-	try {
-		Translate trans;
-		trans.GetNum(); //считываем число для перевода
-		trans.TranslateToEight();
+	try{
+		setlocale(LC_ALL, "rus");
+		int num;
+		Set<int>TSet(1);
+		int delNum;
+		TSet.PrintSet();
+		system("pause");
+		cout << "Enter element to delete:" << endl;
+		cin >> delNum;
+		Set<int>PODSet;
+		PODSet - delNum;
+		TSet.PrintSet();
+		
+		cout << endl << "Set 2 is multiplicity for Set 1: ";
+		if (TSet > PODSet)
+			cout << "YES" << endl;
+		else cout << "NO" << endl;
+		cout << "Set 1 and Set 2 are not equal: ";
+		if (TSet != PODSet)
+			cout << "YES";
+		else cout << "NO";
+		cout << endl;
+		cin >> num;
+		TSet + num;
+		cout << endl;
+		TSet.PrintSet();
+		system("pause");
 	}
-	catch (Translate::TranslateEx) //ловим исключение
-	{
-		cout << "\nВведено некорректное значение!";
+	catch (Set<int>::NORMALNO_POIMAL) {
+		cout << "ZDOROVENNI ILI MALENKI";
+	}
+	catch (Set<int>::bI) {
+		cout << "NETY SETA";
+	}
+	catch (Set<int>::YAZs) {
+		cout << "NO YAAAAZZZZ";
 	}
 	return 0;
 }
+
+//int main() {
+//
+//	try {
+//		Translate trans;
+//		trans.GetNum(); //Г±Г·ГЁГІГ»ГўГ ГҐГ¬ Г·ГЁГ±Г«Г® Г¤Г«Гї ГЇГҐГ°ГҐГўГ®Г¤Г 
+//		trans.TranslateToEight();
+//	}
+//	catch (Translate::TranslateEx) //Г«Г®ГўГЁГ¬ ГЁГ±ГЄГ«ГѕГ·ГҐГ­ГЁГҐ
+//	{
+//		cout << "\nГ‚ГўГҐГ¤ГҐГ­Г® Г­ГҐГЄГ®Г°Г°ГҐГЄГІГ­Г®ГҐ Г§Г­Г Г·ГҐГ­ГЁГҐ!";
+//	}
+//	return 0;
+//}
 
 
